2daaca77e59651a9de2aac7614f6e45d2fa673fa
Nourhan Singer <nourhansingerr7@gmail.com>
2026-06-09 22:01:37 +0300

    add Course and Trainee models with migrations

--- traniee/models.py
+++ traniee/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+from course.models import Course
+
+class Trainee(models.Model):
+
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=100 , null=False, blank=False)
+    courses = models.ManyToManyField(Course)
+
+    def __str__(self):
+        return self.name
+    
